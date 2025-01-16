93fd023f6333c7cc8735a08d86cf5de754ca222f
fatima ezzahra <adblafatmtalzhra@gmail.com>
2025-01-16 20:44:34 +0100

    modification espace offre

---
+++ ErecrTest/Migrations/20250116173331_addOffres.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErecrTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class addOffres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
